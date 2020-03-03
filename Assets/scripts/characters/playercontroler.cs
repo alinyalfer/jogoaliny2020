8cb4623f2e2de33cec7405b22e928d2e487f49f9
alinyalfer <0304aliny@gmail.com>
2020-03-03 11:33:52 -0300

    Creep following Nexus

--- Assets/scripts/characters/playercontroler.cs
+++ Assets/scripts/characters/playercontroler.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-
+[RequireComponent(typeof(NavMeshAgent))]
 public class playercontroler : MonoBehaviour
 {
     NavMeshAgent agent;
     Camera cam;
     public LayerMask walkableLayer;
 
-    void Start()
+    void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         cam = Camera.main;
