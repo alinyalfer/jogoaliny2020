a007a5e991d78174518ca4d10af74634ae570e66
alinyalfer <0304aliny@gmail.com>
2020-03-12 10:40:02 -0300

    Events

--- Assets/scripts/characters/CreepController.cs
+++ Assets/scripts/characters/CreepController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using NavGame.Core;
+using NavGame.Managers;
 
 [RequireComponent(typeof(NavMeshAgent))]
 
@@ -20,6 +21,7 @@ public class CreepController : CombatGameObject
         {
             finalTarget = obj.GetComponent<DamageableGameObject>();
         }
+        onAttackHit += PlayEffects;
 
     }
 
@@ -42,5 +44,9 @@ public class CreepController : CombatGameObject
         {
             agent.SetDestination(finalTarget.transform.position);
         }
+    }
+    void PlayEffects(Vector3 position)
+    {
+        AudioManager.instance.Play("enemy-hit", position);
     }
 }
