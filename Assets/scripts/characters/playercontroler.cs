673f184920752bc7c9242783f1ebbb1392fdd2c4
alinyalfer <0304aliny@gmail.com>
2020-04-25 12:55:31 -0300

    ThrowerGameObject

--- Assets/scripts/characters/playercontroler.cs
+++ Assets/scripts/characters/playercontroler.cs
@@ -10,17 +10,11 @@ public class playercontroler : TouchableGameObject
     NavMeshAgent agent;
     Camera cam;
     public LayerMask walkableLayer;
-    public GameObject prefab;
 
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         cam = Camera.main;
-        GameObject obj = GameObject.FindWithTag("Finish");
-        DamageableGameObject dgo = obj.GetComponent<DamageableGameObject>();
-        GameObject projectile = Instantiate(prefab, transform.position, Quaternion.identity) as GameObject;
-        ProjectileController controller = projectile.GetComponent<ProjectileController>();
-        controller.Init(dgo, 20);
     }
 
     void Update()
